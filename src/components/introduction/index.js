e5a1a1c8379de2f8ec72ea92d10358221977c236
topdirector1100@gmail.com <topdirector1100@gmail.com>
2021-10-16 02:16:48 +0300

    on progress of thread and reaction showing

--- src/components/introduction/index.js
+++ src/components/introduction/index.js
@@ -9,6 +9,8 @@ const Introduction = () => {
           {/* <h1 className="yellow">Bullish.</h1>
           <h1>Are you?</h1> */} 
           <p className="intro-small">Mutant Ether Babies NFT Collecion</p>
+          <p className="intro-small">Nutant Ether Babies</p>
+          <p className="intro-small">NFT Collection Gaming-Metaverse</p>
           <button className="get-llama-btn">Mint Mebs Now</button>
           <div className="intro-steps">
             <span className="ticked">Minted Mebs</span>
