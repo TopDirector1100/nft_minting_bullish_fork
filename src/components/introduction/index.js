c3073d5a5eee5a4aa8c877ba71dc503d6f5d9394
topdirector1100@gmail.com <topdirector1100@gmail.com>
2021-10-16 04:03:31 +0300

    on progress of thread and reaction showing

--- src/components/introduction/index.js
+++ src/components/introduction/index.js
@@ -3,22 +3,20 @@ import "./style.scss"
 const Introduction = () => {
   return (
     <div className="introduction">
-      <div className="intro-box">
-        <div className="intro-inner">
-          {/* <h1 className="yellow">We are</h1>
-          {/* <h1 className="yellow">Bullish.</h1>
-          <h1>Are you?</h1> */} 
-          <p className="intro-small-first">Mutant Ether Babies</p>
-          <p className="intro-small-second">NFT Collection Gaming-Metaverse</p>
-          
-          <button className="get-llama-btn">Mint Mebs Now</button>
-          <div className="intro-steps">
-            <span className="ticked">Minted Mebs</span>
-            <span>Available Mebs</span>
+      <div className="intro-container">
+        <div className="intro-box">
+          <div className="intro-inner">
+            <p className="intro-small-first">Mutant Ether Babies</p>
+            <p className="intro-small-second">NFT Collection Gaming-Metaverse</p>
+            <button className="get-llama-btn">Mint Mebs Now</button>
+            <div className="intro-steps">
+              <span className="ticked">Minted Mebs</span>
+              <span>Available Mebs</span>
+            </div>
           </div>
         </div>
+        <div className="empty-box"></div>
       </div>
-      <div className="empty-box"></div>
     </div>
   )
 
